69e09d1c4a930a37a5952f0be30dcf85b66f7a56
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-08-24 18:42:09 -0400

    Added playRound function to determine the winner and increment the score

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-  const randomNum = Math.random();
+  let randomNum = Math.random();
 
   if (randomNum < 1 / 3) {
     return "rock";
@@ -10,11 +13,48 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 function getHumanChoice() {
-  const userChoice = prompt("Rock, paper, or scissors?");
+  let userChoice = prompt("Rock, paper, or scissors?");
   return userChoice;
 }
 
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+  //const humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1;
+    return "Computer wins! Paper beats rock.";
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    return "Tie!";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore += 1;
+    return "Human wins! Rock beats scissors.";
+  } else if (humanChoice === "rock" && computerChoice === "rock") {
+    return "Tie!";
+  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+    return "Tie!";
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore += 1;
+    return "Computer wins! Rock beats scissors.";
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore += 1;
+    return "Human wins! Scissors beats paper.";
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore += 1;
+    return "Computer wins! Scissors beats paper.";
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanChoice += 1;
+    return "Human wins! Paper beats rock.";
+  } else {
+    return "Invalid response!";
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
