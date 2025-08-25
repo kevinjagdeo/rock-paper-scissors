d801264fb7e7be021cdbf721362ad8aa2831816b
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-08-24 23:17:32 -0400

    Added playGame function to play the game 5 times. Cleaned up previous functions

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   let randomNum = Math.random();
 
@@ -17,44 +14,69 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let userChoice = prompt("Rock, paper, or scissors?");
-  return userChoice;
+  return userChoice.toLowerCase();
 }
 
 //console.log(getHumanChoice());
 
 function playRound(humanChoice, computerChoice) {
-  //const humanChoice = humanChoice.toLowerCase();
+  //returns -1 for tie or invalid, 0 for human win and 1 for computer win
 
   if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore += 1;
-    return "Computer wins! Paper beats rock.";
+    alert("Computer wins! Paper beats rock.");
+    return 1;
   } else if (humanChoice === "paper" && computerChoice === "paper") {
-    return "Tie!";
+    alert("Tie!");
+    return -1;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    humanScore += 1;
-    return "Human wins! Rock beats scissors.";
+    alert("Human wins! Rock beats scissors.");
+    return 0;
   } else if (humanChoice === "rock" && computerChoice === "rock") {
-    return "Tie!";
+    alert("Tie!");
+    return -1;
   } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    return "Tie!";
+    alert("Tie!");
+    return -1;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    computerScore += 1;
-    return "Computer wins! Rock beats scissors.";
+    alert("Computer wins! Rock beats scissors.");
+    return 1;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    humanScore += 1;
-    return "Human wins! Scissors beats paper.";
+    alert("Human wins! Scissors beats paper.");
+    return 0;
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    computerScore += 1;
-    return "Computer wins! Scissors beats paper.";
+    alert("Computer wins! Scissors beats paper.");
+    return 1;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    humanChoice += 1;
-    return "Human wins! Paper beats rock.";
+    alert("Human wins! Paper beats rock.");
+    return 0;
   } else {
-    return "Invalid response!";
+    alert("Invalid response!");
+    return -1;
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  let round = 0;
 
-console.log(playRound(humanSelection, computerSelection));
+  while (round < 5) {
+    const computer = getComputerChoice();
+    const human = getHumanChoice();
+
+    let roundWinner = playRound(human, computer);
+    if (roundWinner === 0) {
+      humanScore++;
+    } else if (roundWinner === 1) {
+      computerScore++;
+    }
+    alert("Scores are human:" + humanScore + " and computer:" + computerScore);
+    round++;
+  }
+
+  alert(
+    "FINAL SCORES ARE human:" + humanScore + " and computer:" + computerScore
+  );
+}
+
+console.log(playGame());
